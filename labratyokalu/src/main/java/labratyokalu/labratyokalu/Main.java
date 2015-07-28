f3c26c26ecf4b34664f2cc97f647bf205965db58
tvkangas <tuukka.kangas@helsinki.fi>
2015-07-28 22:16:23 +0300

    gitignore

---
+++ labratyokalu/src/main/java/labratyokalu/labratyokalu/Main.java
@@ -0,0 +1,10 @@
+
+package labratyokalu.labratyokalu;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei projekti!");
+    }
+    
+}
